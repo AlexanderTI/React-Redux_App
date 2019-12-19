c77a9e5fb345fc17e6b427144bd23754c07aeedf
Oleksandr_Titov <oleksandr_titov@epam.com>
2019-12-19 15:06:04 +0200

    login LS parse

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,25 +4,52 @@ export class LoginForm extends React.Component {
     constructor() {
         super();
         this.state = {
-            isSignIn: true
+            isSignIn: true,
+            loginValue: '',
+            passwordValue: ''
         }
     }
+
+    saveCred = () => {
+        if(!localStorage.getItem('users')) {
+            localStorage.setItem('users', JSON.stringify([]))
+        }
+        let usersArray = JSON.parse(localStorage.getItem('users'));
+        let userObj = {
+            loginValue: this.state.loginValue,
+            passwordValue: this.state.passwordValue
+        };
+        usersArray.push(userObj);
+        localStorage.setItem('users', JSON.stringify(usersArray))
+    };
+
+    checkAccount = () => {
+        if(!localStorage.getItem('users')) {
+            return null
+        }
+        let usersArray = JSON.parse(localStorage.getItem('users'));
+        return  usersArray.find(el => el.loginValue === this.state.loginValue && el.passwordValue === this.state.passwordValue)
+    };
 
     onSwitch = () => {
         this.setState(prevState => ({
-            isSignIn: !prevState.isSignIn 
+            isSignIn: !prevState.isSignIn
         }));
-    }
+    };
 
-    onSubmit = (ev) => {
+    onChange = ev => this.setState({[ev.target.name]: ev.target.value});
+
+    onSubmit = ev => {
         ev.preventDefault();
-        if(this.state.isSignIn){
-            console.log('ENTER')
+        if(!this.state.isSignIn && this.state.loginValue && this.state.passwordValue) {
+            this.saveCred(this.state);
         } else {
-            console.log("ADDED TO LocalStorage")
+            console.log(this.checkAccount());
         }
-    }
-
+        this.setState(
+            {loginValue: '',
+                passwordValue:''})
+    };
 
 
     render() {
@@ -31,11 +58,11 @@ export class LoginForm extends React.Component {
                 <h1 className="login__heading">{this.state.isSignIn ? 'Log in' : 'Sign up' }</h1>
                 <button className="login__switch-form" onClick={this.onSwitch}>{this.state.isSignIn ? 'I don\'t have an account' : 'I have an account'}</button>
                 <form className="login__login-form">
-                    <input type="text" placeholder="Login" />
-                    <input type="text" placeholder="Password" />
-                    <button className="login__submit" onClick={this.onSubmit}>{this. state.isSignIn ? 'Log in' : 'Sign up'}</button>
+                    <input type="text" name="loginValue" placeholder="Login" onChange={this.onChange} value={this.state.loginValue} />
+                    <input type="text" name="passwordValue" placeholder="Password" onChange={this.onChange} value={this.state.passwordValue} />
+                    <button className="login__submit" onClick={this.onSubmit} disabled={!(this.state.loginValue && this.state.passwordValue)}>{this. state.isSignIn ? 'Log in' : 'Sign up'}</button>
                 </form>
             </div>
         )
     }
-};
+}
